953b6bd052746fff48d38323ede270f297af60a3
XUQING <751736277@qq.com>
2019-11-30 23:42:26 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Node-github-hooks
+
+使用 Node.js 实现的简易的 Github Webhooks 服务端
+
+
+
+## 使用方法
+
+1. clone 本项目
+
+2. 修改配置文件 `config.json`：
+
+    ```JSON
+    {
+      "clone_path": "repos",
+      "listen_port": 80,
+      "secret_token": "token",
+      "repo": {
+        "xxxuuu.github.io": {
+          "branch": "master",
+          "shell": "./run.sh"
+        }
+      }
+    }
+    
+    ```
+
+    说明：
+
+    - `clone_path`：clone 到的本地地址，默认值为 `repos`，则项目 a 会 clone 到运行目录下的 `repos/a`
+    - `listen_port`：服务端监听端口
+    - `secret_token`：加密密钥，为保证安全、务必设置
+    - `repo`：仓库，只有在这配置过的仓库才会作相应，没有配置的仓库就算接收到 Github Webhooks 也不作处理
+        - repo 下为 `仓库名:配置`
+        - `brach`：从该仓库拉取的分支
+        - `shell`：拉取完后执行的 shell，可空
+
+3. 使用 `npm run server` 运行
+
+4. 在 Github 仓库 -> Settings -> Webhooks 中 Add webhook 并配置
+
---
+++ index.js
@@ -0,0 +1,123 @@
+let http = require('http');
+let querystring = require('querystring');
+let config = require('./config');
+let exec = require('child_process').exec;
+let crypto = require('crypto');
+let fs = require('fs');
+
+// 默认clone路径
+let gitClonePath = './';
+// 默认端口
+let listenPort = 80;
+
+/**
+ * 初始化配置
+ */
+let initConfig = function() {
+    gitClonePath = config['clone_path'] || gitClonePath;
+    listenPort = config['listen_port'] || listenPort;
+};
+
+/**
+ * SHA1加密
+ * @param str 待加密字符串
+ * @param token 密钥
+ * @return {string} 加密后字符串
+ */
+let sha1 = function(str, token) {
+    return crypto.createHmac('sha1', token).update(str).digest('hex');
+};
+
+/**
+ * 执行命令并默认输出
+ * @param command 执行的命令
+ */
+let simpleExec = function(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (err, stdout, stderr) => {
+            if (err) {
+                console.log(stderr);
+            } else {
+                console.log(stdout);
+            }
+            resolve();
+        });
+    });
+};
+
+/**
+ * 拉取远程仓库
+ * @param clonePath 本地clone地址
+ * @param cloneUrl 远程仓库clone url
+ * @param repoName 仓库名
+ * @param branch 远程仓库分支
+ */
+let cloneRepo = async function(clonePath, cloneUrl, repoName, branch) {
+    let repoPath = clonePath + '/' + repoName;
+    let pullCommand = 'cd ' + repoPath + ' && git pull origin ' + branch + ':master';
+    // 存在就pull，不存在clone
+    try {
+        fs.statSync(repoPath + '/.git/').isDirectory()
+    } catch (e) {
+        await simpleExec('git clone '  + cloneUrl + ' ' + repoPath);
+        return;
+    }
+    await simpleExec(pullCommand);
+};
+
+
+initConfig();
+http.createServer((req, res) => {
+    if(req.method !== 'POST') {
+        return;
+    }
+
+    let dataList = [];
+
+    req.on('data', buffer => {
+        dataList.push(buffer);
+    });
+    req.on('end', () => {
+        // 接收到的数据二进制流
+        let data = Buffer.concat(dataList).toString();
+        // payload数据
+        let payload = JSON.parse(querystring.parse(data)['payload']);
+        // 仓库名和clone地址
+        let repoName = payload['repository']['name'];
+        let cloneUrl = payload['repository']['clone_url'];
+        // 密钥
+        let secret = req.headers['x-hub-signature'];
+
+        // 校验密钥
+        if(secret !== ('sha1='+ sha1(data, config['secret_token']))) {
+            return;
+        }
+
+        // 配置中没有这个仓库
+        if(!config.repo[repoName]) {
+            return;
+        }
+
+        (async () => {
+            console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+            console.log('[%s] %s正在部署...', new Date().toLocaleString(), repoName);
+
+            // 拉取
+            console.log('[%s] 拉取项目中...', new Date().toLocaleString());
+            await cloneRepo(gitClonePath, cloneUrl, repoName, config.repo[repoName]['branch']);
+            console.log('[%s] 拉取完毕', new Date().toLocaleString());
+
+            // 执行shell
+            if(config.repo[repoName]['shell']) {
+                console.log('[%s] 执行shell %s中...', new Date().toLocaleString(), config.repo[repoName]['shell']);
+                await simpleExec('sh ' + config.repo[repoName]['shell']);
+                console.log('[%s] 执行shell 完毕', new Date().toLocaleString());
+            }
+
+            console.log('[%s] 仓库%s部署完毕:)', new Date().toLocaleString(), repoName);
+            console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+        })();
+    });
+    res.end();
+}).listen(listenPort);
+
